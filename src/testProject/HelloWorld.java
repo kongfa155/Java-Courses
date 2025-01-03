11a3ba6754038d8697b094a5e494040e63a94967
kongfa155 <mongpha90@gmail.com>
2025-01-03 18:39:12 +0700

    init all project

---
+++ src/testProject/HelloWorld.java
@@ -0,0 +1,13 @@
+package testProject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world with Java and CP");
+		System.out.println("Hello world with Java and CP");
+		System.out.println("Hello world with Java and CP");
+		System.out.println("Hello world with Java and CP");
+		System.out.println("Hello world with Java and CP");
+	}
+
+}
